9ba8f3313ffad0fb228e4a268b64df71ea3d019b
Lori Burleson <lori.burleson@gmail.com>
2016-10-10 14:35:09 -0400

    Testing index working

--- public/app.js
+++ public/app.js
@@ -1,12 +1,12 @@
-$.ajax({
-  method: 'GET',
-  url: '/api/tasks',
-  dataType: 'json'
-}).done(function(tasks) { // when this request is done, execute this function:
-  $name.text(tasks)
-  console.log(tasks) // prints the full object to the console
-}).fail(function() {
-  console.log('help me')
-})
-
-var $tasks = $('#tasks')
+// $.ajax({
+//   method: 'GET',
+//   url: '/api/tasks',
+//   dataType: 'json'
+// }).done(function(tasks) { // when this request is done, execute this function:
+//   $name.text(tasks)
+//   console.log(tasks) // prints the full object to the console
+// }).fail(function() {
+//   console.log('help me')
+// })
+//
+// var $tasks = $('#tasks')
