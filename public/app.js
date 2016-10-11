d3f8f9ad031b3efbab1f640ed3a49190dba39947
Lori Burleson <lori.burleson@gmail.com>
2016-10-10 22:09:38 -0400

    Can return number of tasks as bullets, but not task data is returned. Also some css updates

--- public/app.js
+++ public/app.js
@@ -1,4 +1,5 @@
 var $tasks = $('#tasks')
+var $boards = $('#boards')
 
 var $taskBoard = $('#board-name')
 var $taskName = $('#task-name')
@@ -13,17 +14,28 @@ $.ajax({
 }).done(function(tasks) { // when this request is done, execute this function:
   var all_boards = tasks.map(function(task) { return task.board })
   var unique_boards = $.unique(all_boards)
-  var taskNames = tasks.map(function(task) { return task.name; })
-  $tasks.text("Your Current Boards: " + unique_boards.join(', ') + ".")
+  // var taskNames = tasks.map(function(task) { return task.name })
+  $boards.text(unique_boards.join(', '))
 }).fail(function() {
   console.log('help me')
 })
+
+// $.ajax({
+//   method: 'GET',
+//   url: '/api/tasks',
+//   dataType: 'json'
+// }).done(function(tasks) {
+//   var all_tasks = tasks.map(function(task) { return task.name })
+//   $tasks.text(all_tasks.join(', '))
+// }).fail(function() {
+//   console.log('help me')
+// })
 
 $("#add-task").on('click', function createTask() {
   var description = $taskDescription.val();
   var board = $taskBoard.val();
   var name = $taskName.val();
-  var label = $('[name=label]:checked').val(); // don't need to save this as a cached value because we want it to be changeable
+  var label = $('[name=label]:checked').val();
 
   $('#task-description').val('');
   $('#board-name').val('');
@@ -39,5 +51,22 @@ $("#add-task").on('click', function createTask() {
       label: label
     }
   })
-
 })
+
+$.ajax({
+  method: 'GET',
+  url: '/api/tasks',
+  dataType: 'json'
+}).done(function(tasks) {
+  var name = $taskName.val();
+  var mapped_tasks = tasks.map(function(task) {
+    var task_item = $( "<li>" ).text( $( name ).text( ) ).get( 0 );
+    return task_item
+  })
+  $tasks.append( mapped_tasks );
+})
+
+    // for each task in tasks
+    // take each content of task.name
+    // append a li
+    // to the ul with id "tasks"
